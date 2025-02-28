e6f9e8f12ba2a22934c421d1bba4f57d409c0b43
wlt <906785275@qq.com>
2025-02-28 14:18:02 +0800

    1

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ import styles from './page.module.css'
 import { Inter } from 'next/font/google'
 import { useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   const [isDrawing, setIsDrawing] = useState(false)
   const [currentTopic, setCurrentTopic] = useState<string | null>(null)
